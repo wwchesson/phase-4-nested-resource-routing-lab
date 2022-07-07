d490689ee5b56b800f6f14b0ebeca5baacd2ca7a
Will <willchesson@gmail.com>
2022-07-07 16:56:02 -0400

    commit tree 9659887a1035ff970f3afb69c6b1395de23a17bb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,43 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
   def index
-    items = Item.all
+    if params[:user_id]
+      users = find_user
+      items = users.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+
+  def show
+    item = find_item
+    render json: item
+  end
+
+  def create
+    user = find_user
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def find_item
+    Item.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Item not found" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
 end
